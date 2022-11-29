54ede4efee13c8c1a68fd0bb6657e468c2d84f3b
jongwon <jwlee9939@gmail.com>
2022-11-30 08:32:41 +0900

    영속성 관리 - 내부 동작 방식 > 영속성 컨텍스트 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -31,13 +31,25 @@ public class JpaMain {
 			*/
 
 			// 조회
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
+			/*List<Member> result = em.createQuery("select m from Member as m", Member.class)
 					.setFirstResult(5)
 					.setMaxResults(8)
 					.getResultList();
 			for (Member member : result) {
 				System.out.println("member.name = " + member.getName());
-			}
+			}*/
+
+			// 비영속
+			Member member = new Member();
+			member.setId(100L);
+			member.setName("HelloJPA");
+
+			// 영속
+			System.out.println("=== BEFORE ===");
+			// member 정보가 바로 데이터베이스에 등록되지 않고 tx.commit 시점에 등록
+			// 애플리케이션과 데이터베이스 사이에 영속성 컨텍스트라는 하나의 영역이 존재
+			em.persist(member);
+			System.out.println("=== AFTER ===");
 
 			tx.commit();
 		} catch (Exception e) {
